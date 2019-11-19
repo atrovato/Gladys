150227e1901b151dcef1c70fd36ef17a64d30777
atrovato <1839717+atrovato@users.noreply.github.com>
2019-11-19 19:04:45 +0100

    Remote box: set value

--- front/src/components/boxs/remote-control/RemoteControlBox.jsx
+++ front/src/components/boxs/remote-control/RemoteControlBox.jsx
@@ -10,6 +10,22 @@ import { DASHBOARD_BOX_STATUS_KEY, DASHBOARD_BOX_DATA_KEY, RequestStatus } from 
   actions
 )
 class RemoteControlBoxComponent extends Component {
+
+  setValue(featureName) {
+    const boxData = get(this.props, `${DASHBOARD_BOX_DATA_KEY}Remote.${this.props.x}_${this.props.y}`);
+    const feature = boxData.remote.features.find(f => f.type === featureName);
+
+    if (feature) {
+      this.props.setValue(feature.selector, 0);
+    }
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.setValue = this.setValue.bind(this);
+  }
+
   componentDidMount() {
     this.props.getRemoteControl(this.props.box, this.props.x, this.props.y);
   }
@@ -39,7 +55,7 @@ class RemoteControlBoxComponent extends Component {
         loading={boxStatus === RequestStatus.Getting}
         editionMode={false}
         remoteType={props.box.remoteType}
-        onClick={props.setValue}
+        onClick={this.setValue}
         remoteName={remoteName}
         featureByType={featureByType}
         dashboard={true}
