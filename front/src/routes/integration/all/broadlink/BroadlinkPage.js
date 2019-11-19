a6504c25fdc7583f5bb50d3b0a12b3823c572540
atrovato <1839717+atrovato@users.noreply.github.com>
2019-11-19 21:58:43 +0100

    Broadlink: fix icons

--- front/src/routes/integration/all/broadlink/BroadlinkPage.js
+++ front/src/routes/integration/all/broadlink/BroadlinkPage.js
@@ -19,7 +19,7 @@ const BroadlinkPage = ({ children, ...props }) => (
                     class="list-group-item list-group-item-action d-flex align-items-center"
                   >
                     <span class="icon mr-3">
-                      <i class="fe fe-target" />
+                      <i class="fe fe-radio" />
                     </span>
                     <Text id="integration.broadlink.remoteTab" />
                   </Link>
@@ -30,7 +30,7 @@ const BroadlinkPage = ({ children, ...props }) => (
                     class="list-group-item list-group-item-action d-flex align-items-center"
                   >
                     <span class="icon mr-3">
-                      <i class="fe fe-radio" />
+                      <i class="fe fe-wifi" />
                     </span>
                     <Text id="integration.broadlink.peripheralTab" />
                   </Link>
